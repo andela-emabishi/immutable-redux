d11fc822c769faf6ed7098f4137f633c90bc631e
Elizabeth Mabishi <elizabeth.wakio@andela.com>
2017-05-07 00:42:40 +0300

    [Feature] Create ADD_REVIEW case for reducer

--- src/reducers/immutableJS/reviews.js
+++ src/reducers/immutableJS/reviews.js
@@ -1,1 +1,27 @@
-import { List, Set } from 'Immutable'
+import { List, Set, Map } from 'Immutable'
+
+const reviews = (state=List(), action) => {
+  switch (action.type) {
+    case 'ADD_REVIEW':
+      const newReview = Map(
+        { id: action.id,
+          item_id: action.item_id,
+          reviewer: action.reviewer,
+          text: action.text,
+          rating: action.rating,
+          flag: false
+        }
+      )
+      return state.push(newReview);
+    case 'DELETE_REVIEW':
+      return state.filter(review => review.id !== action.id);
+    case 'FLAG_REVIEW':
+      return state.map(review => review.id === action.id ? Object.assign({}, review, { flag: action.flag}): review)
+    case 'RATE_REVIEW':
+      return state.map(review => review.id === action.id ? {...review, rating: action.rating }: review)
+    default:
+      return state;
+  }
+}
+
+export default reviews;
