037af037f4b392ebc51da482ff654b8915f8f448
Elizabeth Mabishi <elizabeth.wakio@andela.com>
2017-05-06 15:36:16 +0300

    [Chore] Remove unneccessary paren in flag review statement

--- reducers/es5/reviews.js
+++ reducers/es5/reviews.js
@@ -2,7 +2,7 @@ const reviews = (state=[], action) => {
   switch (action.type) {
     case 'ADD_REVIEW':
     return [
-      ...state, { 
+      ...state, {
         id: action.id,
         reviewer: action.reviewer,
         text: action.text,
@@ -13,9 +13,9 @@ const reviews = (state=[], action) => {
     case 'DELETE_REVIEW':
     return state.filter(review => review.id !== action.id);
     case 'FLAG_REVIEW':
-    return state.map((review, index) => review.id === action.id ? Object.assign({}, review, { flag: !action.flag}): review)
+    return state.map(review => review.id === action.id ? Object.assign({}, review, { flag: action.flag}): review)
     case 'RATE_REVIEW':
-    return state.map((review, index) => review.id === action.id ? [...review, {rating: action.rating}]: review)
+    return state.map(review => review.id === action.id ? [...review, {rating: action.rating}]: review)
     default:
     return state;
   }
