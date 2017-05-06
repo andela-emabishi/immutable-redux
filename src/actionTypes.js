0e9ee92777a0554761adfefc4b346949f78f5812
Elizabeth Mabishi <elizabeth.wakio@andela.com>
2017-05-07 01:00:01 +0300

    [Chore] Create actionTypes file

---
+++ src/actionTypes.js
@@ -0,0 +1,10 @@
+const types = {
+  reviews: {
+    ADD_REVIEW: 'ADD_REVIEW',
+    DELETE_REVIEW: 'DELETE_REVIEW',
+    FLAG_REVIEW : 'FLAG_REVIEW',
+    RATE_REVIEW: 'RATE_REVIEW'
+  }
+}
+
+export default types;
