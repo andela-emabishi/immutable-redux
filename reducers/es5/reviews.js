f65ffd7f7e458519ef78153016b4eb9ff20ebf99
Elizabeth Mabishi <elizabeth.wakio@andela.com>
2017-05-06 16:13:49 +0300

    [Chore] Edit RATE_REVIEW statement

--- reducers/es5/reviews.js
+++ reducers/es5/reviews.js
@@ -1,23 +1,23 @@
 const reviews = (state=[], action) => {
   switch (action.type) {
     case 'ADD_REVIEW':
-    return [
-      ...state, {
-        id: action.id,
-        reviewer: action.reviewer,
-        text: action.text,
-        rating: action.rating,
-        flag: false
-      }
-    ]
+      return [
+        ...state, {
+          id: action.id,
+          reviewer: action.reviewer,
+          text: action.text,
+          rating: action.rating,
+          flag: false
+        }
+      ]
     case 'DELETE_REVIEW':
-    return state.filter(review => review.id !== action.id);
+      return state.filter(review => review.id !== action.id);
     case 'FLAG_REVIEW':
-    return state.map(review => review.id === action.id ? Object.assign({}, review, { flag: action.flag}): review)
+      return state.map(review => review.id === action.id ? Object.assign({}, review, { flag: action.flag}): review)
     case 'RATE_REVIEW':
-    return state.map(review => review.id === action.id ? [...review, {rating: action.rating}]: review)
+      return state.map(review => review.id === action.id ? {...review, rating: action.rating }: review)
     default:
-    return state;
+      return state;
   }
 }
 
